ebdd29c5c6b98f766aae50cbcd6a1952d3cc4fd5
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-08-01 00:41:12 +0530

    Create Interesting_Alphabet.cpp

---
+++ PATTERN/Interesting_Alphabet.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+    /*  Read input as specified in the question.
+     * Print output as specified in the question.
+     */
+    int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+    	char startingch= 'A' + n - i;
+        int j = 1;
+        while(j<=i){
+            char ch = startingch + j - 1;
+            cout<<ch;
+            j++;
+            ch++;
+        }
+        
+        cout<<endl;
+        i++;
+    }
+
+}
+/*
+Code : Interesting Alphabets
+Send Feedback
+Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+Input format :
+N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+8
+Sample Output 1:
+H
+GH
+FGH
+EFGH
+DEFGH
+CDEFGH
+BCDEFGH
+ABCDEFGH
+Sample Input 2:
+7
+Sample Output 2:
+G
+FG
+EFG
+DEFG
+CDEFG
+BCDEFG
+ABCDEFG
+*/
