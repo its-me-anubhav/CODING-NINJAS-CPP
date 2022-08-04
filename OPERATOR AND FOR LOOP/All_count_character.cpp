21f4f0ad8cb9c69b07b88241da87e0d58457ee11
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-08-04 21:33:25 +0530

    Create All_count_character.cpp

---
+++ OPERATOR AND FOR LOOP/All_count_character.cpp
@@ -0,0 +1,47 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+ char c;
+ c = cin.get();
+ //int count = 0;
+ int cb = 0;
+ int num = 0;
+ int cv = 0;
+ while ( c != '$'){
+     //count++;
+     if(c >= 'a' && c<='z'){   // For all the smaller character
+        cb++; 
+     }
+     else if(c>='0' && c<='9'){ // For all the numbers present
+         num++;
+     }
+     else if(c==9 || c==10 ||c==32){  // For spaces ,tab and newline
+         cv++;
+     }
+     c = cin.get();
+ }
+ //cout << count << cb  << num << cv << endl;
+ cout << cb <<" ";
+ cout << num << " ";
+ cout << cv << endl;
+}
+/*
+Count Characters
+Send Feedback
+Write a program to count and print the total number of characters (lowercase english alphabets only), digits (0 to 9) and white spaces (single space, tab i.e. '\t' and newline i.e. '\n') entered till '$'.
+That is, input will be a stream of characters and you need to consider all the characters which are entered till '$'.
+Print count of characters, count of digits and count of white spaces respectively (separated by space).
+Input Format :
+A stream of characters terminated by '$'
+Output Format :
+3 integers i.e. count_of_characters count_of_digits count_of_whitespaces (separated by space)
+Sample Input :
+abc def4 5$
+Sample Output :
+6 2 2
+Sample Output Explanation :
+Number of characters : 6 (a, b, c, d, e, f)
+Number of digits : 2 (4, 5)
+Number of white spaces : 2 (one space after abc and one newline after 4)
+*/
