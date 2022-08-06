9eeea7e7b421484ebbf2ce0f4754a47a3c5d06fa
Anubhav Shakya <106881580+its-me-anubhav@users.noreply.github.com>
2022-08-06 08:31:23 +0530

    Create Decimal_to_Binary.cpp

---
+++ OPERATOR AND FOR LOOP/Decimal_to_Binary.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long decimal, binary = 0, remainder, product = 1;
+  cin >> decimal;
+  while (decimal != 0) {
+    remainder = decimal % 2;
+    binary = binary + (remainder * product);
+    decimal = decimal / 2;
+    product *= 10;
+  }
+  cout << binary ;
+  return 0;
+}
+/*
+Decimal to Binary
+Send Feedback
+Given a decimal number (integer N), convert it into binary and print.
+The binary number should be in the form of an integer.
+Note: The given input number could be large, so the corresponding binary number can exceed the integer range. So you may want to take the answer as long for CPP and Java.
+
+
+Note for C++ coders: Do not use the inbuilt implementation of "pow" function. The implementation of that function is done for 'double's and it may fail when used for 'int's, 'long's, or 'long long's. Implement your own "pow" function to work for non-float data types.
+
+
+Input format :
+Integer N
+Output format :
+Corresponding Binary number (long)
+Constraints :
+0 <= N <= 10^5
+Sample Input 1 :
+12
+Sample Output 1 :
+1100
+Sample Input 2 :
+7
+*/
